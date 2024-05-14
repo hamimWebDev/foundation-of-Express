e5a1f684ee0078816bb8cad42cc17ac5893ecf87
hamimDev <mdhamimhowladerasif@gmail.com>
2024-05-14 07:41:19 +0600

    asynchronous read and wright dada

--- async.js
+++ async.js
@@ -1,1 +1,15 @@
+const fs = require("fs");
 
+fs.readFile("./texts/read.txt", "utf-8", (err, data) => {
+  if (err) {
+    throw Error("Error reading text");
+  } else {
+    console.log(data);
+  }
+
+  fs.writeFile("./texts/read2.txt", data, "utf-8", (err) => {
+    if (err) {
+      throw Error("Error Writhing text");
+    }
+  });
+});
--- index.js
+++ index.js
@@ -13,3 +13,5 @@ console.log(
     "local-1.js"
   )
 );
+
+
